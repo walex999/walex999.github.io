f77c25c44b2534fb0c6eb6ce22dae5bfe1befa9e
walex999 <sayadapro@gmail.com>
2024-04-11 01:12:10 +0200

    more modifications

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function redirect(link) {
     window.location.href = link;
 }
+
+function scrollToSection(event) {
+    event.preventDefault(); // Prevent default link behavior
+    const targetId = this.getAttribute('href'); // Get the target section id
+    const targetSection = document.querySelector(targetId); // Get the target section
+    console.log(targetId,targetSection);
+    if (targetSection) {
+        // Scroll to the target section with smooth behavior
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+}
+
+// Add event listeners to navbar links
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', scrollToSection);
+});
